7296a4b255c03aeb01ccc1be6fd572381dbefb35
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-21 01:42:24 -0300

    feat: add Router and Route with determinated path

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
 import './App.scss';
-import Sidebar from './components/Sidebar'
+
+import Login from './pages/login'
+import AddEvent from './pages/addEvent'
+import ListEvents from './pages/listEvents'
+import Dashboard from './pages/dashboard'
+import MyTickets from './pages/myTickets'
+import MyWallet from './pages/myWallet'
+
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='containerApp'>
-      <Sidebar />
-    </div>
+    <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/addEvent' element={<AddEvent />}/>
+      <Route path='/allEvents' element={<ListEvents />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/myTickets' element={<MyTickets />}/>
+      <Route path='/myWallet' element={<MyWallet />}/>
+    </Routes>
   );
 }
 
