dfff3db3896a3448e224f02f34d6380da0ea08d5
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-22 13:45:46 -0300

    feat: prepare NavButton to more one type of style

--- src/components/NavButton/index.tsx
+++ src/components/NavButton/index.tsx
@@ -3,10 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import { ButtonHTMLAttributes } from 'react'
 
 interface LabeledValue extends ButtonHTMLAttributes<HTMLButtonElement>{
+  children: any;
   route: string;
+  id_event?: number;
 }
 
-function NavButton({route, children }: LabeledValue){
+function NavButton({route, children, id_event }: LabeledValue){
 
   const navigate = useNavigate() 
 
@@ -14,7 +16,7 @@ function NavButton({route, children }: LabeledValue){
     navigate(route)
   }
   return(
-      <button className='btn' onClick={handleRouterButtons}>
+      <button className={`${children === 'Detalhes' ? 'detailsBurron' : 'btn'}`} onClick={handleRouterButtons}>
         {children}
       </button>
   )
