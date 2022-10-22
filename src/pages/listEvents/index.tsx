a78aacb86548f8436b8016eb549b0b8f7c732955
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-22 00:31:49 -0300

    feat: import components to listEvent and add style

--- src/pages/listEvents/index.tsx
+++ src/pages/listEvents/index.tsx
@@ -1,12 +1,20 @@
 import './style.scss';
 import Sidebar from '../../components/Sidebar'
+import CardEvent from '../../components/CardEvent';
+import teste from '../../assets/images/teste.svg'
 
 function ListEvents() {
   return (
     <div className='containerApp'>
       <Sidebar />
-      <section className='hero'>
-        <h1>Listando todos eventos...</h1>
+      <section className='contentPage'>
+        <div>
+          <h1>Listando todos eventos...</h1>
+         
+        </div>
+        <div className='containerEvents'>
+          <CardEvent eventName='Formatura' imgSrc={teste} eventDate='18 de outubro de 2022'></CardEvent>
+        </div>
       </section>
     </div>
   );
