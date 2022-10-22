e54a24a11af95077b44e460c224fa6bcf9c82349
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-22 13:08:14 -0300

    feat: add CardEvent component in map function

--- src/pages/listEvents/index.tsx
+++ src/pages/listEvents/index.tsx
@@ -1,19 +1,43 @@
 import './style.scss';
 import Sidebar from '../../components/Sidebar'
 import CardEvent from '../../components/CardEvent';
-import teste from '../../assets/images/teste.svg'
+
+import {default as dataEvents} from '../../events.json'
+import { useState } from 'react'
+
+interface ICardEventProps{
+  id_event: number;
+  eventName:string;
+  imgSrc:string;
+  eventDate:string;
+}
+
 
 function ListEvents() {
+
+  const [event] = useState<ICardEventProps[]>(dataEvents);
+
   return (
     <div className='containerApp'>
       <Sidebar />
       <section className='contentPage'>
         <div>
           <h1>Listando todos eventos...</h1>
-         
         </div>
         <div className='containerEvents'>
-          <CardEvent eventName='Formatura' imgSrc={teste} eventDate='18 de outubro de 2022'></CardEvent>
+          <>
+            {
+              event.map((data) => 
+                  <CardEvent 
+                    key={data.id_event}
+                    id_event={data.id_event}
+                    eventName={data.eventName}
+                    imgSrc={data.imgSrc} 
+                    eventDate={data.eventDate}
+                  />         
+              )
+            }
+          </>
         </div>
       </section>
     </div>
