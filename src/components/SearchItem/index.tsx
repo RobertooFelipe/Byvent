1432413baa59af30bd9fb30c60b0504f8737534c
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-23 04:22:18 -0300

    feat: create a filter in SearchItem

--- src/components/SearchItem/index.tsx
+++ src/components/SearchItem/index.tsx
@@ -2,11 +2,17 @@ import './style.scss'
 
 import search from '../../assets/images/icon_search.svg'
 
+interface ISearchItemProps{
+  onChange: (inputValue:string)=>void;
+}
 
-function SearchItem(){
+function SearchItem({ onChange }:ISearchItemProps){
   return(
   <div className="serachContainer">
-    <input type="text" />
+    <input 
+      type="text" 
+      onChange={(inputValue) => onChange(inputValue.target.value)}
+    />
     <button className="buttonSearch">
     <img src={search} alt="Serch" />
     </button>
