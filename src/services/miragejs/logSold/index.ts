7a956bf2cd9a2222b9e41e7fcb14800edf8d56c1
RobertooFelipe <feliperoberto092@gmail.com>
2022-10-24 01:18:37 -0300

    feat: create API to get a exemple data

---
+++ src/services/miragejs/logSold/index.ts
@@ -0,0 +1,19 @@
+import { createServer } from "miragejs"
+import log from './logSold.json'
+
+
+export function makeServer() {
+  return createServer({
+    seeds(server) {
+      server.db.loadData({ log })
+    },
+    routes() {
+      this.namespace = "/api"
+      this.timing = 750;
+
+      this.get("/log", (schema) => {
+        return schema.db.log
+      })
+    },
+  })
+}
